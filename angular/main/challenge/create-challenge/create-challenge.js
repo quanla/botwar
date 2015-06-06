85448c033a45d8be78bccdad5884a7c16377c961
Le Anh Quan <quanla2003@gmail.com>
2015-06-06 17:47:20 +0700

    Create challenge

--- angular/main/challenge/create-challenge/create-challenge.js
+++ angular/main/challenge/create-challenge/create-challenge.js
@@ -17,13 +17,14 @@
             ;
         }])
         
-        .controller("create-challenge.ctrl", function($scope, User) {
+        .controller("create-challenge.ctrl", function($scope, User, PositionGenerator, BotSource) {
             User.loadUserBots().then(function(bots) {
                 $scope.bots = bots;
                 $scope.myChampion = bots[0];
+                $scope.oppoBot = bots[0];
             });
-            $scope.showCodeEditor = false;
 
+            $scope.showCodeEditor = false;
 
             $scope.changeBot = function(bot) {
                 $scope.myChampion = bot;
@@ -36,18 +37,88 @@
                     count: 1
                 }
             ];
+            $scope.oppoUnits = [
+                {
+                    type: "footman",
+                    count: 1
+                }
+            ];
 
-            $scope.addUnit = function(unit) {
-                if (unit.count >= 10) return;
-                unit.count ++;
-            };
+            function createGame(myBot, oppoBot) {
 
-            $scope.removeUnit = function(unit) {
-                if (unit.count <= 1) return;
-                unit.count --;
+                var sides = [];
+
+                function addSide(sideNum, unitConfigs, bot) {
+                    var units = [];
+                    var positions = PositionGenerator.generatePositions(sideNum, unitConfigs);
+                    for (var j = 0; j < unitConfigs.length; j++) {
+                        var unitConfig = unitConfigs[j];
+                        for (var k = 0; k < unitConfig.count; k++) {
+                            units.push({
+                                type: unitConfig.type,
+                                position: positions(),
+                                direction: sideNum*Math.PI + Math.PI / 2,
+                                bot: bot==null ? null: BotSource.createBot(bot.code)
+                            });
+                        }
+                    }
+                    sides.push({
+                        color: sideNum == 0 ? "blue" : "red",
+                        units: units
+                    });
+                }
+
+                addSide(0, $scope.oppoUnits, oppoBot);
+                addSide(1, $scope.myUnits, myBot);
+                $scope.game = {
+                    sides: sides
+                };
             }
+
+            createGame();
+
+            $scope.$watch("myUnits", function() { createGame(); }, true);
+            $scope.$watch("oppoUnits", function() { createGame(); }, true);
+
+            $scope.testFight = function() {
+                createGame($scope.myChampion, $scope.oppoBot);
+            }
+
         })
 
+        .directive("unitsSelector", function() {
+            return {
+                restrict: "E",
+                scope: {
+                    "units": "="
+                },
+                templateUrl: "angular/main/challenge/create-challenge/units-selector.html",
+                link: function($scope, elem, attrs) {
+
+                    $scope.addUnit = function(unit) {
+                        if (unit.count >= 5) return;
+                        unit.count ++;
+                    };
+
+                    $scope.removeUnit = function(unit) {
+                        if (unit.count <= 1) return;
+                        unit.count --;
+                    };
+
+                }
+            };
+        })
+
+        .factory("PositionGenerator", function() {
+            return {
+                generatePositions: function(side, unitConfigs) {
+                    var i=0;
+                    return function() {
+                        return {x: side*300 + 100, y: 150 + (i++) * 50};
+                    }
+                }
+            };
+        })
     ;
 
 })();
