e2c102d6f85433e29894525c6e7e71809a2dbf95
Le Anh Quan <quanla2003@gmail.com>
2015-06-09 11:23:24 +0700

    Localhost check

--- angular/main/main.js
+++ angular/main/main.js
@@ -13,9 +13,14 @@
         'ui.bootstrap'
     ])
 
-        .config(["ApiProvider", function (ApiProvider) {
-            //ApiProvider.setHost("http://54.254.246.157:1006");
-            ApiProvider.setHost("http://192.168.1.70:1006");
-        }])
+        .config(function (ApiProvider) {
+
+            if (window.location.href.indexOf("http://localhost:") == 0) {
+                ApiProvider.setHost("http://192.168.1.70:1006");
+                //ApiProvider.setHost("http://54.254.246.157:1006");
+            } else {
+                ApiProvider.setHost("http://54.254.246.157:1006");
+            }
+        })
     ;
 })();
